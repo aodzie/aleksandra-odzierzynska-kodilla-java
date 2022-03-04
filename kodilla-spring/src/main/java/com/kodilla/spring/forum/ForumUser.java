6e890ee7b49deaa9ee7b18f45fe1440f54c94ca0
Aleksandra <ola.odzierzynska@gmail.com>
2022-03-04 16:35:48 +0100

    17

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -15,17 +15,4 @@ public class ForumUser {
         return username;
     }
 
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ForumUser forumUser = (ForumUser) o;
-        return Objects.equals(username, forumUser.username);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(username);
-    }
 }
