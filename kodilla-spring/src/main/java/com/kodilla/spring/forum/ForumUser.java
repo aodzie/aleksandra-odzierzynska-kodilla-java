3a5b157f1629307300e87481747a6d9d766652ae
Aleksandra <ola.odzierzynska@gmail.com>
2022-03-04 16:30:21 +0100

    17

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -2,6 +2,8 @@ package com.kodilla.spring.forum;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 @Component
 public class ForumUser {
     String username;
@@ -11,5 +13,19 @@ public class ForumUser {
 
     public String getUsername() {
         return username;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ForumUser forumUser = (ForumUser) o;
+        return Objects.equals(username, forumUser.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
     }
 }
