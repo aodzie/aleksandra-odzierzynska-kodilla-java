f9b1d96004e362f5105af6c4d8e531127d21b9d7
Aleksandra Odzierzynska <ola.odzierzynska@gmail.com>
2022-05-16 12:55:11 +0200

    30.3. Procedury i funkcje – informacje uzupełniające

--- kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
@@ -5,32 +5,32 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public enum DbManager {
+public class DbManager {
 
-    INSTANCE;                                                  // [1]
+    private Connection conn;
+    private static DbManager dbManagerInstance; // [2]
 
-    private Connection conn;                                   // [2]
-
-    DbManager() {                                              // [3]
+    private DbManager() throws SQLException {                                              // [3]
         Properties connectionProps = new Properties();          // [4]
         connectionProps.put("user", "kodilla_user");            // [5]
-        connectionProps.put("password", "kodilla_Pass123");     // [6]
-        try {
-            conn = DriverManager.getConnection(                  // [7]
-                    "jdbc:mysql://localhost:3306/kodilla_course" +
-                            "?serverTimezone=Europe/Warsaw" +
-                            "&useSSL=False",                                  // [10]
-                    connectionProps);                                 // [11]
-        } catch (SQLException e) {                              // [12]
-            throw new ExceptionInInitializerError(e);            // [13]
-        }                                                       // [14]
+        connectionProps.put("password", "kodilla_password");     // [6]
+        conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/task_crud" +
+                        "?serverTimezone=Europe/Warsaw" +
+                        // "&useSSL=False" +
+                        "&allowPublicKeyRetrieval=true",                                  // [10]
+                connectionProps);                                 // [11]         // [14]
     }                                                          // [15]
 
-    public static DbManager getInstance() {                    // [16]
-        return INSTANCE;                                        // [17]
+    public static DbManager getInstance() throws SQLException{                    // [16]
+        if (dbManagerInstance == null){
+            dbManagerInstance= new DbManager();
+        }
+        return dbManagerInstance;
     }                                                          // [18]
 
-    public Connection getConnection() {                        // [19]
+    public Connection getConnection() throws SQLException{                        // [19]
         return conn;                                            // [20]
-    }                                                          // [21]
+    }
+    // [21]
 }
